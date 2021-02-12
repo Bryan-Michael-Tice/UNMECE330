4ed11a51ec74831fbf7ced098f3be5e9dda32439
root <root@DESKTOP-76RAP40.localdomain>
2021-02-12 00:50:16 -0700

    completed static functions

---
+++ Module01/Static/main.c
@@ -0,0 +1,36 @@
+#include "matrix_static.h"
+
+int main() 
+{
+	    static T data[] = {1,2,3,4};
+            int rows,cols;
+	    matrix  a,b,userDefined;
+            a = create_initvals(2,2,data);
+            b = create_empty(2,2);
+	    equate(&a,&b);
+            printf("\n Matrix a:");
+	    matrix_print(a);
+	    printf("\n Matrix b:");
+	    matrix_print(b);
+	    printf("\n a+b:");
+	    matrix_print(add(a,b));
+
+	    printf("\n\033[1;31m Transposed Matrix a \033[0m:");
+	    matrix_print_int(transpose(a));
+
+	    printf("\n\033[1;31m Transposed Matrix b  \033[0m:");
+	    matrix_print_int(transpose(b));
+
+	    printf("\n\033[1;34m Enter Number of Rows  \033[0m:");
+	    scanf("%d",&userDefined.row_dim);
+
+	    printf("\n\033[1;34m Enter number of Columns  \033[0m:");
+	    scanf("%d",&userDefined.col_dim);
+	    
+	    userDefined = create_userDefined(userDefined.row_dim,userDefined.col_dim);
+	 
+	    printf("\n\033[1;31m Transposed Matrix Defined by user \033[0m:");
+	    matrix_print_int(transpose(userDefined));
+}
+
+
